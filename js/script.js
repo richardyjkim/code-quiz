d38b5a76793f5763d54f831443f9a05e0e6bbaf1
Richard <richardyjkim@gmail.com>
2021-02-19 00:47:41 -0800

    question for loop added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
 let startBtn = document.getElementById("start");
 let timerEl = document.getElementById("countdown");
+let titleEl = document.getElementById("title");
 let scoreBtn = document.getElementById("highScore");
+let descriptionEl = document.getElementById("description");
 
+let answerListItems = document.getElementsByClassName("answer-list-item");
+
+let quizQuestions = [
+  {
+    "question": "Commonly used data type DO not Include:",
+    "answers": ["String", "Boolean", "alerts", "numbers"],
+    "correctAnswer": "alerts"
+  },
+  {
+    "question": "Commonly used data type DO not Include:",
+    "answers": ["String", "Boolean", "alerts", "numbers"],
+    "correctAnswer": "alerts"
+  },
+  {
+    "question": "Commonly used data type DO not Include:",
+    "answers": ["String", "Boolean", "alerts", "numbers"],
+    "correctAnswer": "alerts"
+  },
+  {
+    "question": "Commonly used data type DO not Include:",
+    "answers": ["String", "Boolean", "alerts", "numbers"],
+    "correctAnswer": "alerts"
+  },
+  {
+    "question": "Commonly used data type DO not Include:",
+    "answers": ["String", "Boolean", "alerts", "numbers"],
+    "correctAnswer": "alerts"
+  },
+
+];
 
 function countdown() {
-  var timeLeft = 100;
-  var timeInterval = setInterval(function() {
+  let timeLeft = 100;
+  let timeInterval = setInterval(function() {
     if (timeLeft > 1) {
       
       timerEl.textContent = "Time: " + timeLeft ;
@@ -25,10 +57,26 @@ function countdown() {
     }
   }, 1000);
 
+};
 
+let loadQuestions = function(index) {
+  let questionJson = quizQuestions[index];
+  let answers = questionJson["answers"];
+
+  for (let i = 0 ; i < answerListItems.length; i++) {
+    answerListItems[i].textContent = answers[i];
+    answerListItems[i].classList.remove("display-none");
+  }
+  console.log(answerListItems);
 }
 
+startBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  descriptionEl.textContent = "";
+  titleEl.textContent = "Commonly used data type DO not Include:"
+  startBtn.classList.add("display-none"); 
 
-
+  loadQuestions(0);
+});
 
 startBtn.onclick = countdown;
