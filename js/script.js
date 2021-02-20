872ec16e6d98c9d110f4f108a16c26a327f4b332
Richard <richardyjkim@gmail.com>
2021-02-19 22:02:46 -0800

    css added

--- js/script.js
+++ js/script.js
@@ -3,34 +3,34 @@ let timerEl = document.getElementById("countdown");
 let titleEl = document.getElementById("title");
 let scoreBtn = document.getElementById("highScore");
 let descriptionEl = document.getElementById("description");
-
 let answerListItems = document.getElementsByClassName("answer-list-item");
+let feedbackEl = document.getElementById("feedback");
 
 let quizQuestions = [
   {
     "question": "Commonly used data type DO not Include:",
-    "answers": ["String", "Boolean", "alerts", "numbers"],
-    "correctAnswer": "alerts"
+    "answers": ["String", "Boolean", "Alerts", "Numbers"],
+    "correctAnswer": "Alerts"
   },
   {
-    "question": "Commonly used data type DO not Include:",
-    "answers": ["String", "Boolean", "alerts", "numbers"],
-    "correctAnswer": "alerts"
+    "question": "The condition in an if / else statement is enclosed with___________.",
+    "answers": ["quotes", "curly brackets", "parenthesis", "squre brackets"],
+    "correctAnswer": "parenthesis"
   },
   {
-    "question": "Commonly used data type DO not Include:",
-    "answers": ["String", "Boolean", "alerts", "numbers"],
-    "correctAnswer": "alerts"
+    "question": "A very useful tool used during devlopment and debugging for printing content to the debugger is:",
+    "answers": ["Javascript", "terminal/bash", "for loops", "console.log"],
+    "correctAnswer": "console.log"
   },
   {
-    "question": "Commonly used data type DO not Include:",
-    "answers": ["String", "Boolean", "alerts", "numbers"],
-    "correctAnswer": "alerts"
+    "question": "Array in Javascript can be used to store__________.",
+    "answers": ["numers and strings", "other arrays", "booleans", "all of the above"],
+    "correctAnswer": "all of the above"
   },
   {
-    "question": "Commonly used data type DO not Include:",
-    "answers": ["String", "Boolean", "alerts", "numbers"],
-    "correctAnswer": "alerts"
+    "question": "String values must be enclosed within _________ when being assigned to variables.",
+    "answers": ["commas", "curly brackets", "quotes", "parenthesis"],
+    "correctAnswer": "parenthesis"
   },
 
 ];
@@ -43,18 +43,7 @@ function countdown() {
       timerEl.textContent = "Time: " + timeLeft ;
     
       timeLeft--;
-    } else if (timeLeft === 1) {
-      
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-    
-      timerEl.textContent = '';
-      
-      clearInterval(timeInterval);
-    
-      displayMessage();
-    }
+    } 
   }, 1000);
 
 };
@@ -62,13 +51,29 @@ function countdown() {
 let loadQuestions = function(index) {
   let questionJson = quizQuestions[index];
   let answers = questionJson["answers"];
+  let questions = questionJson["question"];
 
   for (let i = 0 ; i < answerListItems.length; i++) {
     answerListItems[i].textContent = answers[i];
     answerListItems[i].classList.remove("display-none");
+    titleEl.textContent = questions
+    
+    // add eventlistner
+    answerListItems[i].addEventListener("click", function(event){
+      let currentListItem = event.currentTarget;
+      let answersText = currentListItem.innerText;
+      if (answersText === questionJson["correctAnswer"]) {
+        feedbackEl.textContent = "Correct!"
+      } else {
+        countdown -= 10;
+        feedbackEl.textContent = "Wrong!"
+      }
+      loadQuestions(i);
+    })
   }
-  console.log(answerListItems);
-}
+};
+
+
 
 startBtn.addEventListener("click", function(event) {
   event.preventDefault();
